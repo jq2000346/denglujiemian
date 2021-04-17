14bee2a229d5f66519c005b5f10c628669452f21
190470117jiangqin <1484985137@qq.com>
2021-04-17 14:07:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/denglujiemian/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.denglujiemian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextWatcher;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+
+
+
+	}
+}
